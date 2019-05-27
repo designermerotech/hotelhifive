c0f90994492ede3549325bb011d2449fca18eccc
designermerotech <designer.merotech@gmail.com>
2019-05-27 15:40:48 +0545

    initial commit

---
+++ admin/db.php
@@ -0,0 +1,5 @@
+<?php
+// $con = mysqli_connect("pdb28.awardspace.net","2771015_hotel","hotel@hi5","2771015_hotel") or die(mysqli_error());
+$con = mysqli_connect("localhost","root","","2771015_hotel") or die(mysqli_error());
+
+?>
